bfc3f257764e8f9da823b4dc96859f724e8ee5ac
CODE\mail <mail@code.dk>
2018-08-02 15:18:27 +0200

    Debug done

---
+++ Assets/CheckWhatIsBought.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CheckWhatIsBought : MonoBehaviour
+{
+
+	public FirstPersonController PlayerFPS;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (PlayerPrefs.GetInt("SpeedBoost") == 1)
+		{
+			PlayerFPS.m_RunSpeed = 24;
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
